88806ae9244a230afc1a23c2a739898ba2e40fa1
Richard Marques <richardblas31@gmail.com>
2026-03-05 00:04:07 -0300

    Atividade de Escopo de váriavel técnica.

---
+++ Escopo-de-váriavel-Java/Questão2.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 1; i++) {
+            int variavelLocal = 100;
+            System.out.println(variavelLocal);
+        }
+    }
+}
